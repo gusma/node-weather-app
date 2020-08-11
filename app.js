6fdf5a43ab4d92a54ed0bd3996973341f9e5e797
root <root@ar-it11106.globant.com>
2020-08-11 04:01:53 -0300

    Add shorthand & destructuring

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Boston', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
+const address = process.argv[2]
+
+if (!address) {
+    console.log('Please provide an address!')
+}
+
+else{ 
+geocode(address, (error, {latitude, longitude, location} = {}) => {
+
+    if (error) {
+        return console.log(error)
+    }
+   
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log(location)
+        console.log(forecastData)
+    
+    })
 })
 
-forecast(-34.5949676,-58.4514497, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+}
--- utils/geocode.js
+++ utils/geocode.js
@@ -3,18 +3,18 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiZ3VzbWE4MyIsImEiOiJja2RwZ3F3dGswdm02MnlzYXhub2tnc21iIn0.cTOxL2dYABzaLmQy7zS8jw&limit=1';
 
-    request({url: url, json: true }, (error, response) => {
+    request({url: url, json: true }, (error, { body }) => {
         if(error) {
            callback('Unable to connect to location services, sorry!', undefined);
         }
-        else if(response.body.features.length === 0) {
+        else if(body.features.length === 0) {
             callback('Unable to find the sought address! Sorry!')
         }
         else{
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude: response.body.features[0].center[0], 
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0], 
+                location: body.features[0].place_name
             })
         }
     })
